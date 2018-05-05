e352ae0cf2fdf86a41202a647c6f3f2d500d22b0
Jatin Menghani <Jatin11@192.168.0.4>
2018-05-05 16:30:32 +0530

    Remove CoverFlowLayout

--- CapstoneNews10/Pods/Target Support Files/LNICoverFlowLayout/LNICoverFlowLayout-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LNICoverFlowLayout : NSObject
-@end
-@implementation PodsDummy_LNICoverFlowLayout
-@end
--- CapstoneNews10/Pods/Target Support Files/Pods-CapstoneNews10/Pods-CapstoneNews10-frameworks.sh
+++ CapstoneNews10/Pods/Target Support Files/Pods-CapstoneNews10/Pods-CapstoneNews10-frameworks.sh
@@ -135,13 +135,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/LNICoverFlowLayout/LNICoverFlowLayout.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Protobuf/Protobuf.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/LNICoverFlowLayout/LNICoverFlowLayout.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Protobuf/Protobuf.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
